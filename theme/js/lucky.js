8abcfc80145bd34c9497aec39051b774f450ee73
adamnovotnycom <adamnovotnycom@users.noreply.github.com>
2021-09-18 17:21:34 +0000

    fix lucky.js; add /pages functionality adamnovotnycom/adamnovotny-com@e7b1a025793e88c4fd4933a27c573ee5d492d243

--- theme/js/lucky.js
+++ theme/js/lucky.js
@@ -3,29 +3,21 @@ var app = new Vue({
     el: '#quote',
     data: {
         text: '',
-        url: '',
-        author: ''
-
+        author: '',
+        url: ''
     },
     mounted () {
-        // OLD method
-        // URL https://spreadsheets.google.com/feeds/list/{{sheet_id}}/{{sheet_number}}/public/basic?alt=json
-        // axios
-        //     .get('https://spreadsheets.google.com/feeds/list/1LMv7QPWl1SWZiplsJciO7KnKa_Lvt4TBVnu6igYSguk/3/public/basic?alt=json')
-        //     .then(response => {
-        //         quotes = response['data']['feed']['entry']
-        //         quoteIndex = Math.floor(Math.random() * (quotes.length - 1))
-        //         selectedQuote = quotes[quoteIndex]; // random quote
-        //         this.text = selectedQuote['title']['$t'];
-        //         this.author = selectedQuote['content']['$t'].split(',')[0].split(' ').slice(1,5).join(" ");
-        //         this.url = selectedQuote['content']['$t'].split(',')[1].split(' ')[2];
-        //     })
-
-        // New method
-        axios
-            .get('https://docs.google.com/spreadsheets/d/e/2PACX-1vQdwyyqdYlU6DxxlnTJ07gFbQkwZSArRde6jylWVLBGNVsOc0Z3Gq8FfmEagJRUZOnjgafCd-eA_Fql/pubhtml?gid=1899751390&amp;single=true&amp;widget=true&amp;headers=false')
-            .then(response => {
-                console.log(response)
+        // Credits tutorial : https://benborgers.com/posts/google-sheets-json
+        const spreadsheetId = '1LMv7QPWl1SWZiplsJciO7KnKa_Lvt4TBVnu6igYSguk'
+        fetch(`https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:json&sheet=quotes`)
+            .then(res => res.text())
+            .then(text => {
+                const json = JSON.parse(text.substr(47).slice(0, -2));
+                sheetRows = json.table.rows;
+                rowIndex = Math.floor(Math.random() * (sheetRows.length - 1));
+                this.text = sheetRows[rowIndex].c[0].v;
+                this.author = sheetRows[rowIndex].c[1].v;
+                this.url = sheetRows[rowIndex].c[2].v;
             })
     }
 })

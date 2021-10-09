0bcaaab5599853cd2d115edfcc83783b9d79ae68
adamnovotnycom <adamnovotnycom@users.noreply.github.com>
2021-10-09 17:23:22 +0000

    create a random fact on lucky list adamnovotnycom/adamnovotny-com@94f40dd80ab273ab110f850b950bc13b487d060c

--- theme/js/lucky.js
+++ theme/js/lucky.js
@@ -1,10 +1,11 @@
 
 var app = new Vue({
-    el: '#quote',
+    el: '#luck',
     data: {
-        text: '',
-        author: '',
-        url: ''
+        quote_text: '',
+        quote_author: '',
+        quote_url: '',
+        factLink: ''
     },
     mounted () {
         // Credits tutorial : https://benborgers.com/posts/google-sheets-json
@@ -15,9 +16,18 @@ var app = new Vue({
                 const json = JSON.parse(text.substr(47).slice(0, -2));
                 sheetRows = json.table.rows;
                 rowIndex = Math.floor(Math.random() * (sheetRows.length - 1));
-                this.text = sheetRows[rowIndex].c[0].v;
-                this.author = sheetRows[rowIndex].c[1].v;
-                this.url = sheetRows[rowIndex].c[2].v;
+                this.quote_text = sheetRows[rowIndex].c[0].v;
+                this.quote_author = sheetRows[rowIndex].c[1].v;
+                this.quote_url = sheetRows[rowIndex].c[2].v;
+            })
+        
+        fetch(`https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:json&sheet=links`)
+            .then(res => res.text())
+            .then(text => {
+                const json = JSON.parse(text.substr(47).slice(0, -2));
+                sheetRows = json.table.rows;
+                rowIndex = Math.floor(Math.random() * (sheetRows.length - 1));
+                this.factLink = sheetRows[rowIndex].c[0].v;
             })
     }
 })
